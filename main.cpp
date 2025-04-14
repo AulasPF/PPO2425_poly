bcb74b8f43b16a60b52e2b9355b789c651faf91f
Pedro Fonseca <pf@ua.pt>
2025-04-14 18:08:56 +0100

    Vector de figuras

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <vector>
 #include "Circle.h"
+#include "Figure.h"
 #include "Rectangle.h"
 #include "Triangle.h"
+
 
 using namespace std;
 
 int main() {
+
+    vector <Figure> figuras; 
+
     Figure f({4.0, 4.0});
     Circle c({1.0, 2.0}, 5.0);
     Rectangle r({0.0, 0.0}, 4.0, 6.0);
@@ -16,6 +22,15 @@ int main() {
     cout << c.info() << endl;
     cout << r.info() << endl;
     cout << t.info() << endl;
+
+    figuras.push_back(f); 
+    figuras.push_back(c); 
+    figuras.push_back(r); 
+    figuras.push_back(t); 
+    
+    for( auto fig: figuras){
+        cout << fig.info() << endl; 
+    }
 
     return 0;
 }
